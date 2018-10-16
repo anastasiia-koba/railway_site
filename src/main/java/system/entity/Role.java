70dc133bc43468c5adf795fc3e3138d3a180604b
Anastasiia K <anastaciiakoba@gmail.com>
2018-10-16 10:21:51 +0300

    Changed UserProfile file
    - added dao-annotations
    - added Roles for users
    - added confirm password

---
+++ src/main/java/system/entity/Role.java
@@ -0,0 +1,58 @@
+package system.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Simple JavaBean object that represents role of (@link UserProfile)
+ */
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<UserProfile> users;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserProfile> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<UserProfile> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
+}
--- src/main/java/system/entity/UserProfile.java
+++ src/main/java/system/entity/UserProfile.java
@@ -1,11 +1,47 @@
 package system.entity;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * Simple JavaBean domain object that represents a User
+ */
+
+@Entity
+@Table(name = "users")
 public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "username")
     private String login;
+
+    @Column(name = "password")
     private String pass;
+
+    //private Date birthDate;
+
+    @Transient
+    private String confirmPassword;
+
+    @ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 
     public UserProfile(){
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public UserProfile(String login, String pass) {
@@ -32,5 +68,21 @@ public class UserProfile {
 
     public void setPass(String pass) {
         this.pass = pass;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 }
